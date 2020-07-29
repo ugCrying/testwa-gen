1a01760d470843dd8d792557d29cb2d61b4c92a4
tanghongxin <hongxin.tang@hotmail.com>
2020-07-29 23:21:10 +0800

    🐛(scrcpy) LD_LIBRARY_PATH 动态写入

--- src/api/scrcpy/scrcpy.js
+++ src/api/scrcpy/scrcpy.js
@@ -1,13 +1,15 @@
-const { execSync, spawn, exec } = require('child_process')
+const { execSync, exec } = require('child_process')
 const { argv } = require('yargs')
 
-let isForwarded = false
+const lineBreak = (process.platform === 'win32') ? '\n\r' : '\n';
 
 // 运行时注入的变量
 const { sn } = argv
 
+const LD_LIBRARY_PATH = execSync('adb shell CLASSPATH=/data/local/tmp/scrcpy-server.jar app_process / com.genymobile.scrcpy.Server -L').toString().replace(lineBreak, '')
+
 execSync(`adb -s ${sn} forward tcp:1717 tcp:6612`)
-const command = `adb -s ${sn} shell LD_LIBRARY_PATH=/system/lib64:/vendor/lib64:/data/local/tmp CLASSPATH=/data/local/tmp/scrcpy-server.jar app_process / com.genymobile.scrcpy.Server`
+const command = `adb -s ${sn} shell LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/data/local/tmp CLASSPATH=/data/local/tmp/scrcpy-server.jar app_process / com.genymobile.scrcpy.Server`
 exec(command)
 
 console.log('--------------')
@@ -19,9 +21,7 @@ process.on('close', () => {
   console.log('close')
 })
 
-
-
-process.on('exit', (e) => {
+process.on('exit', () => {
   console.log('exit')
   process.exit()
 })
